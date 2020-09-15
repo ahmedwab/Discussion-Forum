2e5c82e711a46f8dbea5fda50d861ab85c1f374d
ahmedwab <52334883+ahmedwab@users.noreply.github.com>
2020-09-15 17:22:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function change_form(){
+  document.getElementById("sign-up").style.visibility = "visible";
+  document.getElementById("sign-in").style.visibility = "hidden";
+}
+function change_form2(){
+  document.getElementById("sign-up").style.visibility = "hidden";
+  document.getElementById("sign-in").style.visibility = "visible";
+}

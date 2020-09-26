7e6c236c9ea54b457781eed2513d69f555853fa1
ahmedwab <52334883+ahmedwab@users.noreply.github.com>
2020-09-25 23:29:21 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function change_form(){
-  document.getElementById("sign-up").style.visibility = "visible";
-  document.getElementById("sign-in").style.visibility = "hidden";
-}
-function change_form2(){
-  document.getElementById("sign-up").style.visibility = "hidden";
-  document.getElementById("sign-in").style.visibility = "visible";
-}

bdcd69acda1de3fd440ae34c397f8392ecd8d2c0
ahmedwab <52334883+ahmedwab@users.noreply.github.com>
2021-06-02 19:00:40 -0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function showname () {
-      var image = document.getElementById('file-input');
-      document.getElementById('imageposted').innerHTML=image.files.item(0).name+" uploaded";
-    };
